beb17bb1360cad446f3251b1bd0962e28ef6b0a3
Nik Macintosh <nik.macintosh@clio.com>
2020-04-16 20:40:40 -0700

    Integrate React Navigation

--- App.tsx
+++ App.tsx
@@ -1,19 +1,63 @@
-import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { FC } from "react";
 import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
     justifyContent: "center",
   },
 });
+
+const ExerciseScreen: FC = function () {
+  return (
+    <View style={styles.container}>
+      <Text>Exercise</Text>
+    </View>
+  );
+};
+
+const ExerciseStack = createStackNavigator();
+
+const ExerciseStackScreen: FC = function () {
+  return (
+    <ExerciseStack.Navigator>
+      <ExerciseStack.Screen component={ExerciseScreen} name="Exercise" />
+    </ExerciseStack.Navigator>
+  );
+};
+
+const HabitScreen: FC = function () {
+  return (
+    <View style={styles.container}>
+      <Text>Habit</Text>
+    </View>
+  );
+};
+
+const HabitStack = createStackNavigator();
+
+const HabitStackScreen: FC = function () {
+  return (
+    <HabitStack.Navigator>
+      <HabitStack.Screen component={HabitScreen} name="Habit" />
+    </HabitStack.Navigator>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen component={ExerciseStackScreen} name="Exercise" />
+        <Tab.Screen component={HabitStackScreen} name="Habit" />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
