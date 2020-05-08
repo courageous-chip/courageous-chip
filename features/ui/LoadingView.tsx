0cf76f70fb14cfb40ef81da6c7358dfbac88317b
Nik Macintosh <me@knickmack.com>
2020-05-07 22:59:24 -0700

    Add reolving emoji to the loading view to lighten it up

--- features/ui/LoadingView.tsx
+++ features/ui/LoadingView.tsx
@@ -1,13 +1,48 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export const LoadingView: FC = function () {
+  const emoji = useRevolvingEmoji();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>🤔</Text>
+      <Text style={styles.text}>{emoji}</Text>
     </View>
   );
 };
+
+const useRevolvingEmoji = function (delay = 500, seed = "🤔") {
+  const [emoji, setEmoji] = useState(seed);
+
+  useEffect(() => {
+    const id = setInterval(() => setEmoji(getRandomEmoji()), delay);
+
+    return () => clearInterval(id);
+  }, [delay]);
+
+  return emoji;
+};
+
+function getRandomEmoji(
+  set = [
+    "🍕",
+    "🤖",
+    "🧠",
+    "👽",
+    "👾",
+    "🤘",
+    "🖖",
+    "🖕",
+    "🧙‍♂️",
+    "🧝‍♀️",
+    "🧞‍♀️",
+    "🕺",
+    "💃",
+    "🤳",
+  ],
+) {
+  return set[Math.floor(Math.random() * set.length)];
+}
 
 const styles = StyleSheet.create({
   container: {
