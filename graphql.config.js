7901dff0d2a6ee0d8b05e2d485e01e4ef94269cf
Nik Macintosh <me@knickmack.com>
2020-05-08 19:36:23 -0700

    Finish configuration of @testing-library/react-native

---
+++ graphql.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  schema: "./data/schema.graphql",
+};
--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,37 @@
+const jestPreset = require("@testing-library/react-native/jest-preset");
+const expoPreset = require("jest-expo/jest-preset");
+
 module.exports = {
-  // "The above additions let's Jest know to collect coverage of all .js, .jsx,
-  // .ts, & .tsx file types and not within /coverage, /node_modules/ and our 3
-  // project config files (add/remove more exclusions to this list to match your
-  // Expo app needs)."
+  // https://www.native-testing-library.com/docs/install#jest-preset
+  ...expoPreset,
+  ...jestPreset,
+  // "The [below] additions let's Jest know to collect coverage of all .js,
+  // .jsx, .ts, & .tsx file types and not within /coverage, /node_modules/ and
+  // our 3 project config files (add/remove more exclusions to this list to
+  // match your Expo app needs)."
   //
   // https://docs.expo.io/guides/testing-with-jest/#code-coverage-reports
+  //
   collectCoverageFrom: [
     "!**/__generated__/**",
     "!**/.eslintrc.js",
     "!**/@types/**",
+    "!**/apollo.config.js",
     "!**/babel.config.js",
     "!**/coverage/**",
+    "!**/graphql.config.js",
     "!**/jest.config.js",
     "!**/node_modules/**",
     "!**/prettier.config.js",
     "!**/scripts/**",
     "**/*.{js,jsx,ts,tsx}",
   ],
-  // "A Jest preset that mocks out the native side of the Expo SDK and handles
-  // some configuration for you"
+  // You can ensure cleanup is called after each test and import additional
+  // assertions by adding it to the setup configuration in Jest.
   //
-  // https://docs.expo.io/guides/testing-with-jest/#installation
+  // https://www.native-testing-library.com/docs/setup#cleanup
   //
-  preset: "jest-expo",
+  setupFilesAfterEnv: ["@testing-library/react-native/cleanup-after-each"],
   // "A great starting point to make sure any modules you may be using within
   // /node_modules/ are transpiled when running jest. This should cover the
   // majority of your needs but you can always add to this pattern list as you see
